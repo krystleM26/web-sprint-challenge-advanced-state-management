cd1ec3aa959e6df6be75c9e8b5bfc1c15420853d
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-01-21 14:42:26 -0800

    Connected fetchSmurf to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { Component } from "react";
-
+import React, {useEffect } from "react";
+import {connect} from 'react-redux'
 import AddForm from './components/AddForm';
 import SmurfList from './components/SmurfList';
 import Header from './components/Header';
+import { fetchSmurfs } from "./actions";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
-const App = ()=> {
+
+const App = ({dispatch})=> {
+  useEffect(() => {
+    dispatch(fetchSmurfs())
+}, [])
   return (
     <div className="App">
       <Header />
@@ -20,7 +25,13 @@ const App = ()=> {
   );
 }
 
-export default App;
+
+const mapStateToProps= (state)=> {
+  return {
+    smurfs: state.smurfs
+  }
+}
+export default connect(mapStateToProps)(App);
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
--- src/components/SmurfList.js
+++ src/components/SmurfList.js
@@ -1,22 +1,13 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import Smurf from './Smurf'
 import { connect } from 'react-redux'
-import axios from 'axios'
-import { fetchSmurfs } from './../actions/'
+
+
 
 const SmurfList = ({ smurfs, isLoading, error, dispatch }) => {
 
-    useEffect(() => {
-        dispatch(fetchSmurfs())
-    }, [])
-  const testSmurf = {
-    id: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
-    name: 'Poppa Smurf',
-    position: 'Village Leader',
-    nickname: 'Pops',
-    description:
-      'Papa is the practical village leader and the father figure of 100 or so young Smurfs. He is easily identified by his red Smurf hat, pants, and a shortly-trimmed white beard and moustache.',
-  }
+ 
+
 
   if (isLoading) {
     return <h1>Loading...</h1>
